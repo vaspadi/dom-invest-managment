40b1de4d29a5bef91ff90c0430959929d58f934e
Pavel Vasiljev <vasiliev.work1@gmail.com>
2022-02-03 18:23:46 +0200

    Update responsibleImage component

--- components/global/ResponsibleImage.vue
+++ components/global/ResponsibleImage.vue
@@ -1,9 +1,10 @@
 <template>
-  <picture class="responsible-image">
-    <source type="image/jpeg" :srcset="require(`~/assets/img/${ img }?resize`).srcSet">
-    <source type="image/webp" :srcset="require(`~/assets/img/${ img }?resize&format=webp`).srcSet">
-    <img class="responsible-image__image" :src="require(`~/assets/img/${ img }?resize`).src" :alt="alt">
+  <picture v-if="checkLink()" class="responsible-image">
+    <source type="image/webp" :srcset="require(`~/assets/img/${ getRelativeSrc() }?resize&format=webp`)">
+    <img class="responsible-image__image" :src="require(`~/assets/img/${ getRelativeSrc() }?resize`)" :alt="alt">
   </picture>
+
+  <img v-else :src="img" :alt="alt">
 </template>
 
 <script>
@@ -20,6 +21,17 @@ export default {
       type: String,
       default: ''
     }
+  },
+
+  methods: {
+    getRelativeSrc () {
+      // Из админки приходит не относительный путь, а /assets/img/...
+      return this.img ? this.img.replace(/\/assets\/img\//, '') : ''
+    },
+
+    checkLink () {
+      return !(/^http/).test(this.img)
+    }
   }
 }
 </script>
@@ -27,20 +39,14 @@ export default {
 <style lang="scss">
 .responsible-image {
   height: 100%;
+  width: 100%;
   min-height: inherit;
-
-  &_contain {
-    .responsible-image__image {
-      object-fit: contain;
-    }
-  }
 
   &__image {
     width: 100%;
     height: 100%;
     min-height: inherit;
     object-fit: cover;
-    border-radius: inherit;
   }
 }
 </style>
